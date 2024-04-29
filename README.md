98d73af87c08bbfa2ee2dd5f6bf6c5ba255a92bd
rahatfaruk <rahatfaruk99@gmail.com>
2024-04-29 07:48:43 +0600

    mongo server connected; middleware added

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 My assignment 10 of programming hero, b9. Focused on crud using mongodb. Here, in backend, I will do crud operations using mongodb atlas, expressjs. In frontend I have used react.
 
 ## Links:
-  - server repo: https://github.com/programming-hero-web-course-4/b9a10-server-side-rahatfaruk
+  - frontend repo: https://github.com/programming-hero-web-course-4/B9A10-client-side-rahatfaruk 
+  - server repo: https://github.com/programming-hero-web-course-4/b9a10-server-side-rahatfaruk 
--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Welcome')
+const mongoUri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ympa4ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// init mongo client instance 
+const client = new MongoClient(mongoUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true    
+  }
 })
+
+// middlewares
+app.use( cors() )
+app.use( express.json() )
+
+async function run() {
+  try {
+    // connect to server 
+    await client.connect()
+
+    // ref to db and collection
+    const database = client.db('pha10')
+    const spotsCollection = database.collection('spots')
+
+    // ## routes
+    app.get('/', async (req, res) => {
+      const cursor = spotsCollection.find()
+      const data = await cursor.toArray()
+      res.send(data)
+    })
+
+    // Send a ping to confirm a successful connection
+    await database.command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!")
+  } 
+  catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`server listening to http://localhost:${port}`);
